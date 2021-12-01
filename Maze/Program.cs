8f81779f2d6c44b1a3840cd1bb9375d1bb3c94c6
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2021-12-01 12:25:26 +0100

    one more room

--- Maze/Program.cs
+++ Maze/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 
 System.Console.WriteLine("Du är fast i ett rum och du behöver komma ute. Dörr A eller Dör B?");
+Console.ReadLine();
 
-string door ="";
+string door ="A";
 
 while(door != "ute")
 {
+    if (door == "A")
+    {
+        Console.WriteLine("Du är inne i rum A");
+        door = Console.ReadLine();
+        
+    }
 
-
-
+    else if (door == "B")
+    {
+        Console.WriteLine("Du är i rum B");
+        door = Console.ReadLine();
+    }
 }
 
