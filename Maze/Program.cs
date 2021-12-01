8b4318d4c2038bfc6e44ead6f79bc904e8a8c0b9
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2021-12-01 12:36:20 +0100

    chnages

--- Maze/Program.cs
+++ Maze/Program.cs
@@ -3,16 +3,29 @@
 System.Console.WriteLine("Du är fast i ett rum och du behöver komma ute. Dörr A eller Dör B?");
 Console.ReadLine();
 
-string door ="A";
+string door = "A";
 
-while(door != "ute")
+while (door != "ute")
 {
     if (door == "A")
     {
         Console.WriteLine("Du är inne i rum A");
+        Console.WriteLine("Rum C eller D?");
         door = Console.ReadLine();
-        
     }
+
+        if (door == "C")
+        {
+            Console.WriteLine("Du är i rum C");
+            Console.ReadLine();
+        }
+
+        else if (door == "D")
+        {
+            Console.WriteLine("Du är i rum D");
+            door = Console.ReadLine();
+        }
+
 
     else if (door == "B")
     {
